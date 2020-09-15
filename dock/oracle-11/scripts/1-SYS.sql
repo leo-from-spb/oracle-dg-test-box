5a9f623bf9d49cba5039ad0b606e8500ec9328a9
Leonid Bushuev <leonidos@gmail.com>
2020-09-16 00:24:34 +0200

    Init Oracle 11 container

---
+++ dock/oracle-11/scripts/1-SYS.sql
@@ -0,0 +1,36 @@
+---- DEVELOPMENT ROLE ----
+
+create role Development
+/
+
+grant create cluster,
+      create sequence,
+      create type,
+      create table,
+      create view,
+      create materialized view,
+      create trigger,
+      create procedure,
+      create operator,
+      create indextype,
+      create dimension,
+      create library,
+      create synonym,
+      create database link,
+      alter database link
+to Development
+/
+
+
+---- REMOVE STUPID "SECURITY" ----
+
+alter profile default limit password_life_time unlimited
+/
+
+alter profile default
+    limit failed_login_attempts unlimited
+    password_life_time unlimited
+/
+
+alter system set sec_case_sensitive_logon = false  -- starts from Oracle 10
+/
