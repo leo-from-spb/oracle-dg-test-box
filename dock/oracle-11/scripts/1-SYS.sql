af7889e48e24e41533faa5799beaea575e2f4125
Leonid Bushuev <leonid.bushuev@jetbrains.com>
2020-09-16 13:33:52 +0200

    Add Guests

--- dock/oracle-11/scripts/1-SYS.sql
+++ dock/oracle-11/scripts/1-SYS.sql
@@ -22,6 +22,7 @@ to Development
 /
 
 
+
 ---- REMOVE STUPID "SECURITY" ----
 
 alter profile default limit password_life_time unlimited
@@ -34,3 +35,60 @@ alter profile default
 
 alter system set sec_case_sensitive_logon = false  -- starts from Oracle 10
 /
+
+
+
+---- CREATE GUEST USERS ----
+
+create user Guest identified by G
+                  default tablespace users
+                  temporary tablespace temp
+                  quota 0 on users
+/
+
+grant connect to Guest
+/
+
+
+
+create user Guest_AC identified by GAC
+                    default tablespace users
+                    temporary tablespace temp
+                    quota 0 on users
+/
+
+grant connect to Guest_AC
+/
+
+grant select_catalog_role to Guest_AC
+/
+
+
+create user Guest_AD identified by GAD
+                    default tablespace users
+                    temporary tablespace temp
+                    quota 0 on users
+/
+
+grant connect to Guest_AD
+/
+
+grant select any dictionary to Guest_AD
+/
+
+
+create user Guest_AX identified by GAX
+                    default tablespace users
+                    temporary tablespace temp
+                    quota 0 on users
+/
+
+grant connect to Guest_AX
+/
+
+grant select_catalog_role to Guest_AX
+/
+
+grant select any dictionary to Guest_AX
+/
+
